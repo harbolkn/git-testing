ac7deaf7956479dd00a369f2ad5e27c4f8bfdaf6
Kris Harbold <harbolkn@gmail.com>
2023-09-09 23:15:41 -0400

    add input to main

--- proto/appA.py
+++ proto/appA.py
@@ -9,12 +9,12 @@ def generate_random_numbers(n):
     return [random.randint(1, 100) for _ in range(n)]
 
 
-def main():
-    numbers = generate_random_numbers(5)
+def main(num):
+    numbers = generate_random_numbers(num)
     squared_numbers = [num ** 2 for num in numbers]
     sum_of_squares = sum(squared_numbers)
     return sum_of_squares
 
 
 if __name__ == "__main__":
-    print(main())
+    print(main(5))
