aa4250fdd29749f5c106c0096dde4a42f7523eaa
Kristof Harbold <harbolkn@gmail.com>
2023-09-10 02:45:40 +0000

    Get field names from keys in first row of data object

--- proto/appB.py
+++ proto/appB.py
@@ -1,14 +1,16 @@
 import csv
 
+
 def read_csv(filepath):
     with open(filepath, 'r') as file:
         reader = csv.reader(file)
         data = list(reader)
     return data
 
+
 def write_csv(filepath, data):
     with open(filepath, mode='w', newline='') as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer = csv.DictWriter(csv_file, fieldnames=data[0].keys())
 
         # Write the header row with field names
         writer.writeheader()
